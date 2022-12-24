0575a666131e2ffbe86d69bc6b391cca20dfa46d
Ayasha <khanayasha190@gmail.com>
2022-12-25 01:27:18 +0530

    fixed password check block

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -6,7 +6,7 @@ public class Email {
     private String password;
 
 
-    public Email(String emailId){
+    public Email(String emailId) {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
@@ -19,57 +19,40 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
-          if(this.password.equals(oldPassword) && charchecker(newPassword) && upperchecker(newPassword) && lowerchecker(newPassword) && digitchecker(newPassword) && specialchecker(newPassword)){
+    public void changePassword(String oldPassword, String newPassword) {
 
-          this.password = newPassword;
-        }
+
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if (this.password.equals(oldPassword)) {
+
+            boolean upperChecker = false, lowerChecker = false, digitChecker = false, specialChecker = false;
+
+            for (int i = 0; i < newPassword.length(); i++) {
+
+                char ch = newPassword.charAt(i);
+
+                if (Character.isUpperCase(ch))
+                    upperChecker = true;
+
+                else if (Character.isLowerCase(ch))
+                    lowerChecker = true;
+
+                else if (Character.isDigit(ch))
+                    digitChecker = true;
+
+                else specialChecker = true;
+
+            }
+            if (newPassword.length() >= 8 && upperChecker && lowerChecker && digitChecker && specialChecker) {
+                this.password = newPassword;
+            }
+        }
     }
-public boolean charchecker(String newPassword){
-        if(newPassword.length()>=8)
-            return true;
-        return false;
 }
-    public boolean upperchecker(String newPassword){
-        for(int i =0;i<newPassword.length();i++){
-            if(Character.isUpperCase(newPassword.charAt(i)){
-                return true;
-            }
-        }
-        return false;
-    }
-    public boolean lowerchecker(String newPassword){
-        for(int i =0;i<newPassword.length();i++){
-            if(Character.isLowerCase(newPassword.charAt(i)){
-                return true;
-            }
-        }
-        return false;
-    }
-    public boolean digitchecker(String newPassword){
-        for(int i =0;i<newPassword.length();i++){
-            if(Character.isDigit(newPassword.charAt(i)){
-                return true;
-            }
-        }
-        return false;
-    }
-    public boolean specialchecker(String newPassword){
-        for(int i =0;i<newPassword.length();i++){
-            if (!Character.isDigit(newPassword.charAt(i))
-                    && !Character.isLetter(newPassword.charAt(i))
-                    && !Character.isWhitespace(newPassword.charAt(i)){
-                return true;
-            }
-            }
-        return false;
-        }
-    }
 
 
