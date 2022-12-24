95cc6c6f35bf25bd3b10da925dcdcc95a237ceff
Ayasha <khanayasha190@gmail.com>
2022-12-24 23:20:38 +0530

    Password function done

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -19,6 +19,10 @@ public class Email {
     }
 
     public void changePassword(String oldPassword, String newPassword){
+          if(oldPassword.equals(newPassword) && charchecker(newPassword) && upperchecker(newPassword) && lowerchecker(newPassword) && digitchecker(newPassword) && specialchecker(newPassword)){
+
+          oldPassword = newPassword;
+        }
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
@@ -26,4 +30,45 @@ public class Email {
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
     }
+public boolean charchecker(String newPassword){
+        if(newPassword.length()>=8)
+            return true;
+        return false;
 }
+    public boolean upperchecker(String newPassword){
+        for(int i =0;i<newPassword.length();i++){
+            if(Character.isUpperCase(newPassword.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean lowerchecker(String newPassword){
+        for(int i =0;i<newPassword.length();i++){
+            if(Character.isLowerCase(newPassword.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean digitchecker(String newPassword){
+        for(int i =0;i<newPassword.length();i++){
+            if(Character.isDigit(newPassword.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean specialchecker(String newPassword){
+        for(int i =0;i<newPassword.length();i++){
+            if (!Character.isDigit(newPassword.charAt(i))
+                    && !Character.isLetter(newPassword.charAt(i))
+                    && !Character.isWhitespace(newPassword.charAt(i)){
+                return true;
+            }
+            }
+        return false;
+        }
+    }
+
+
